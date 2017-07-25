aea75a62741baaf5f0d85502908b14c053590e36
sarahsweat <sarahsweat@gmail.com>
2017-07-25 09:50:32 -0400

    Done.

--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -1,1 +1,18 @@
 // Code Invitation Component Here
+
+import React from 'react'
+
+export default class Invitation extends React.Component {
+  constructor(props){
+    super(props)
+  }
+
+  render () {
+    return (
+      <div>
+        <h1>You've been invited!</h1>
+        {this.props.children}
+      </div>
+    )
+  }
+}
--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -1,1 +1,18 @@
 // Code ThemedDecoration Component Here
+
+
+import React from 'react'
+
+export default class ThemedDecoration extends React.Component {
+  constructor(props){
+    super(props)
+  }
+
+  render () {
+    return (
+      <div>
+        {this.props.children.map(c => <div className={this.props.theme}>{c}</div>)}
+      </div>
+    )
+  }
+}
